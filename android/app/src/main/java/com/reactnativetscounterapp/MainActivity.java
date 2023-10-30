792d1e7e6b8599ad92eb1f533011dcb7b4b4cd30
Abel Borit <abelboritguitton@gmail.com>
2023-10-30 14:00:46 -0500

    rename files from ReactNativeTSCounterApp to ReactNativeTSIntroCourseApp

--- android/app/src/main/java/com/reactnativetscounterapp/MainActivity.java
+++ android/app/src/main/java/com/reactnativetscounterapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnativetscounterapp;
+package com.reactnativetsintrocourseapp;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "ReactNativeTSCounterApp";
+    return "ReactNativeTSIntroCourseApp";
   }
 
   /**
--- ios/ReactNativeTSCounterAppTests/ReactNativeTSCounterAppTests.m
+++ ios/ReactNativeTSCounterAppTests/ReactNativeTSCounterAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ReactNativeTSCounterAppTests : XCTestCase
+@interface ReactNativeTSIntroCourseAppTests : XCTestCase
 
 @end
 
-@implementation ReactNativeTSCounterAppTests
+@implementation ReactNativeTSIntroCourseAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
